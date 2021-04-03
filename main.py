3d2698c28fb82ba56c67dd42ce13dadb80db5e8a
Sabi <sabina.gregusova@gmail.com>
2021-04-03 13:43:48 +0200

    Merging files

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python
 from dataLoader import DataLoader
 
@@ -8,10 +7,3 @@ from dataLoader import DataLoader
 
 dataLoad = DataLoader()
 print(dataLoad.batchProvider())
-=======
-#!/usr/bin/env python
-
-### parsing input arguments here
-
-### create/train/load model
->>>>>>> 5dc3dc99e8a655fac6bc96d205689c439c977e03
