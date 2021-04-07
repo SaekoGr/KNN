ff5b153c62ce0a660f82dc76f9f7a48aa14d5666
Sabína Gregušová <sabina.gregusova@gmail.com>
2021-04-07 14:27:42 +0200

    Hopefully final update for the evaluation

--- src/evaluation_main.py
+++ src/evaluation_main.py
@@ -21,13 +21,11 @@ class EvaluationMetrics:
         self.unionArea = []
         self.intersectionArea = []
         self.dice_coeff = []
-        self.dice_loss = []
 
 
-    def evaluateBatch(self, groundTruth, prediction, groundBbox, predictionBbox):
-        #print("Evaluated model by Pixel accuracy")
+    def evaluateBatch(self, groundTruth, prediction):
         self.pixelAccuracy(groundTruth, prediction)
-        [self.intersectionOverUnion(groundBbox[i], predictionBbox[i]) for i in range(len(groundBbox))]
+        self.intersectionOverUnion(groundTruth, prediction)
 
     def pixelAccuracy(self, y, prediction):
         ones_count = ((np.abs(y - prediction)).sum()).item()
@@ -36,38 +34,12 @@ class EvaluationMetrics:
         self.pixel_acc_err += ones_count
         self.pixel_total_sum += full_shape
 
-    def intersectionOverUnion(self, groundBbox, predictionBbox):
-        #print("IoU")
-        #print("Ground truth " + str(groundBbox))
-        #print("Evaluating by IoU")
-        area1 = abs(groundBbox[3] - groundBbox[1]) * abs(groundBbox[2] - groundBbox[0])
-        #print("Ground truth area " + str(area1))
+    def intersectionOverUnion(self, y, prediction):
+        intersectionArea = (np.logical_and(y ,prediction).sum()).item()
+        unionArea =  (np.logical_or(y, prediction).sum()).item()
 
-        #print("Prediction bbox " + str(predictionBbox))
-        area2 = abs(predictionBbox[3] - predictionBbox[1]) * abs(predictionBbox[2] - predictionBbox[0])
-        #print("Prediction area " + str(area2))
-
-        ### intersection rectangle coordinates
-        x5 = max(groundBbox[1], predictionBbox[1])
-        y5 = max(groundBbox[0], predictionBbox[0])
-        x6 = min(groundBbox[3], predictionBbox[3])
-        y6 = min(groundBbox[2], predictionBbox[2])
-
-        intersectionArea = abs(x6 - x5) * abs(y6 - y5)
-        #print("Intersection area " + str(intersectionArea))
-        unionArea =  area1 + area2 - intersectionArea
         self.unionArea.append(unionArea)
         self.intersectionArea.append(intersectionArea)
-        #iou = intersectionArea / unionArea
-        #print("IoU " + str(iou))
-
-        dice_coefficient = (2*intersectionArea) / (unionArea + intersectionArea)
-
-        #print("Dice coefficient " + str(dice_coefficient))
-        #self.dice_coeff.append(dice_coefficient.item())
-        #self.dice_loss.append((1 - dice_coefficient).item())
-
-        #self.iou.append(iou.item())
 
     def getIoU(self):
         self.iou = np.sum(self.intersectionArea) / np.sum(self.unionArea) * 100
@@ -91,57 +63,6 @@ class EvaluationMetrics:
         print("Intersection over union " + str(round(self.getIoU(), 2)) + "%")
         print("Dice coefficient " + str(round(self.getDiceLoss(), 2)) + "%")
 
-
-def bbox(points):
-    """
-    Calculates bounding box by finding the first nonzero row/column
-
-    return rectangle given by 2 points: (y_min, x_min, y_max, x_max)
-    """
-    x_range, y_range = points.shape
-
-    #print(points.sum())
-
-    x_min = 0
-    x_max = x_range - 1
-    y_min = 0
-    y_max = y_range - 1
-
-    # find x_min
-    #print("XMIN")
-    for i in range(x_range):
-        #print(np.sum(points[i,:]))
-        if np.sum(points[i,:]) > 0.0:
-            x_min = i
-            break
-
-    # find y_min
-    #print("YMIN")
-    for i in range(y_range):
-        #print(np.sum(points[:,i]))
-        if np.sum(points[:,i]) > 0.0:
-            y_min = i
-            break
-
-    ### max needs one correction
-    # find x_max
-    #print("XMAX")
-    for i in range(x_range - 1, -1, -1):
-        #print(np.sum(points[i,:]))
-        if np.sum(points[i,:]) > 0.0:
-            x_max = i
-            break
-
-    #print("YMAX")
-    for i in range(y_range - 1, -1, -1):
-        #print(np.sum(points[:,i]))
-        if np.sum(points[:,i]) > 0.0:
-            y_max = i
-            break
-
-    return (y_min, x_min, y_max, x_max)
-
-
 def evaluate(epoch_id, model, batch_size=4, min_res_size=16):
     threshold=0.5
     gen = batch_generator(batch_size, min_res_size, False)
@@ -159,15 +80,13 @@ def evaluate(epoch_id, model, batch_size=4, min_res_size=16):
             prediction = model(X)
             prediction = (prediction.cuda()).detach().cpu().clone().numpy()
 
-        #print(prediction.shape)
-        thresholded_prediction = np.where(prediction >= threshold, 1.0, 0.0)
         # calculate bounding boxes for thresholded prediction
-        prediction_bboxes = [bbox(thresholded_prediction[i][0]) for i in range(batch_size)]
-
+        thresholded_prediction = np.where(prediction >= threshold, 1.0, 0.0)
+        
         # evaluate this batch
-        evalMetrics.evaluateBatch(y.cpu(), thresholded_prediction, bboxes, prediction_bboxes)
+        evalMetrics.evaluateBatch(y.cpu(), thresholded_prediction)
 
-        del X, y, bboxes, prediction
+        del X, y, prediction, bboxes
 
         gc.collect()
         torch.cuda.empty_cache()
@@ -177,50 +96,3 @@ def evaluate(epoch_id, model, batch_size=4, min_res_size=16):
     evalMetrics.getEvaluation(epoch_id)
 
     return evalMetrics.pixel_acc, evalMetrics.iou, evalMetrics.dice_coeff
-
-if __name__ == "__main__":
-    batch_size = 4
-    threshold = 0.5
-
-    gen = batch_generator(batch_size, 16, False)
-    model = PSPnet() # this will get loaded: load_model()
-    model.to(device)
-
-    batch_n = next(gen)
-
-    evalMetrics = EvaluationMetrics(batch_n)
-
-    model.zero_grad()
-
-    s = perf_counter()
-    for n in range(batch_n):
-        X, y, _, bboxes = next(gen)
-        #print(X.shape, y.shape, bboxes)
-
-        with torch.no_grad():
-            prediction = model(X)
-            prediction = (prediction.cuda()).detach().cpu().clone().numpy()
-
-        #print(prediction.shape)
-        thresholded_prediction = np.where(prediction >= threshold, 1.0, 0.0)
-        # calculate bounding boxes for thresholded prediction
-        prediction_bboxes = [bbox(thresholded_prediction[i][0]) for i in range(batch_size)]
-
-        # evaluate this batch
-        #print("Evaluating")
-        #print(y.shape, prediction.shape)
-        evalMetrics.evaluateBatch(y.cpu(), thresholded_prediction, bboxes, prediction_bboxes)
-
-        del X, y, bboxes, prediction
-
-        gc.collect()
-        torch.cuda.empty_cache()
-        loading(n+1, batch_n)
-        # uncomment for final product
-        break
-
-    print("total time = ", perf_counter() - s)
-    evalMetrics.getEvaluation()
-
-    print("\nEvaluation completed")
-
